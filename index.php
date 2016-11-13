7c0d069517e02b2ac298e733934c2afbe26e5a05
hoper2015 <hoper2015@gmail.com>
2016-11-13 15:34:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<title>Helping Hands</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="stylesheet.css" rel="stylesheet">
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	</head>
 	<body>
+		<header>
+			<img src="helpinghands.png" alt="Helping Hands">
+		</header>
+		<nav>
+			<a class="active" href="index.html">Home</a>
+			<a href="about.html">About</a>
+			<a href="volunteer.html">Volunteer</a>
+			<a href=""></a>
+			<a href=""></a>
+			<a href=""></a>
+		</nav>
+		<main>
+			<h1>Welcome to Helping Hands!</h1>
+			<p>Are you looking to help out your community? There are many things that you can do to help your community. There is no better way to help than to offer a helping hand to a neighbor in need. One day you could need a helping hand and your neighbor may lend you hand back.</p>
+			<p>Check out our <a href="volunteer.html">Volunteer</a> to offer a helping hand to your community.</p>
+			<p>Help your community be a better place!</p>
+			
+			<img src="" alt="">
+		</main>
 	</body>
 </html>
+
